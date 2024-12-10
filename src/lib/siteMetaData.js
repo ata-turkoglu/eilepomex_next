c4e16ffe9e8452eac21c142a046f7a9fa684552e
Ata Türkoğlu <ata.turkoglu@reengen.com>
2024-12-10 13:25:04 +0300

    refactor lang texts

---
+++ src/lib/siteMetaData.js
@@ -0,0 +1,13 @@
+export default {
+    title: "Eile Pomex",
+    description: "Yapı Kimyasalları, Construction Chemicals",
+    headerTitle: "Eile Pomex",
+    theme: "system",
+    siteUrl: "https://www.eilepomex.com",
+    siteLogo: "/assets/logos/eile.png",
+    socialBanner: "/assets/eilepomex-round.png",
+    email: "info@eilepomex.com",
+    linkedin: "https://www.linkedin.com/company/eilepomex",
+    instagram: "https://www.instagram.com/eilepomex",
+    facebokk: "https://www.facebook.com/eilepomex",
+};
