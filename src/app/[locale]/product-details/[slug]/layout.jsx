9c651e59e4feb91699b09ec83cf7d37834af2cb2
Ata Türkoğlu <ata.turkoglu@reengen.com>
2024-12-21 15:50:54 +0300

    add metadata for each product

--- src/app/[locale]/product-details/[slug]/layout.jsx
+++ src/app/[locale]/product-details/[slug]/layout.jsx
@@ -2,6 +2,7 @@ import productList from "@/data/productList.json";
 import { routing } from "@/i18n/routing";
 import { setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
+import { slugify } from "@/utils/commonFuncs";
 
 export async function generateStaticParams({ params }) {
     const { locale } = await params;
@@ -11,54 +12,50 @@ export async function generateStaticParams({ params }) {
         const langs = Object.keys(item.name);
         langs.forEach((key) => {
             list.push({
-                slug:
+                slug: item.id.toString() + "-" + slugify(item.name[key]),
+                /* slug:
                     item.id.toString() +
                     "-" +
                     item.name[key]
-                        .toLocaleLowerCase(locale)
+                        .toLocaleLowerCase(findLocale("en"))
                         .split(" ")
-                        .join("-"),
+                        .join("-"), */
             });
         });
     });
-
     return list;
 }
 
 export async function generateMetadata({ params }) {
-    return productList.map((item) => {
-        return {
-            title: item.name[params.locale],
-            description: item.description[params.locale],
-        };
-    });
+    const { locale, slug } = await params;
+
+    const productId = slug.split("-")[0];
+    const product = productList.find((item) => item.id == productId);
+    const keywords = product.description["tr"].split(" ");
+    return {
+        title: product.name[locale],
+        description: product.description[locale],
+        keywords,
+    };
 }
 
 export default async function ProductDetailsLayout({ children, params }) {
     const { locale, slug } = await params;
 
-    const productId = slug.split("-")[0];
-
     if (!routing.locales.includes(locale)) {
         notFound();
     }
-
-    const product = productList.find((item) => item.id == productId);
-
-    const jsonLd = {
-        title: product.name[locale],
-        description: product.description[locale],
-        keywords: ["sssa", "ataaa"],
-    };
 
     setRequestLocale(locale);
 
     return (
         <>
+            {/*{" "}
             <script
                 type="application/ld+json"
                 dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-            />
+            />{" "}
+            */}
             <div
                 style={{
                     width: "100%",
