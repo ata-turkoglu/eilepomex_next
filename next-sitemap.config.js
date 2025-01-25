f691386347cbe745adeb072cfabcc2d8bbc1dc17
Ata Türkoğlu <ata.turkoglu@reengen.com>
2025-01-25 14:08:01 +0300

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+// Default code you can customize according to your requirements.
+module.exports = {
+    siteUrl: "https://eilepomex.com/",
+    generateIndexSitemap: false,
+    generateRobotsTxt: true, // (optional)
+    // REST CODE READ DOCS  ...
+    changefreq: "monthly",
+};
